73782869c19cbb9f5905ce3e7e6f52ffda7dc1ea
narasimha <narasimha@codebrahma.com>
2017-10-06 18:37:24 +0530

    Initial commit * this is a practice application to learn nodejs * using expressjs * implemented models and routes to perform CURD operations on models

---
+++ libs/middlewares.js
@@ -0,0 +1,15 @@
+import bodyParser from 'body-parser';
+
+module.exports = app => {
+	app.set('port', 3000);
+	app.set('json spaces', 4);
+	app.use(bodyParser.urlencoded({
+	  extended: true
+	}));
+	
+	app.use(bodyParser.json());
+
+	app.use((req, res, next) => {
+		next();
+	})
+}
